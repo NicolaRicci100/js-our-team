6e949eec97a104ea530952851f3c6a507703ee9f
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-12 15:20:52 +0200

    preparo array con objects

--- js/script.js
+++ js/script.js
@@ -9,10 +9,37 @@ console.log('JS OK');
 // Scott Estrada |	Developer            |	scott-estrada-developer.jpg
 // Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg
 
-const data = [
+
+//* array con tutti i dati divisi in objects
+const staff = [
     {
-        name: "",
-        profession: "",
-        photo: "",
+        name: "Wayne Barnett",
+        profession: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        profession: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        profession: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        profession: "Social Media Manager",
+        photo: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        profession: "Developer",
+        photo: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        profession: "Graphic Designer",
+        photo: "barbara-ramos-graphic-designer.jpg",
     },
 ];
